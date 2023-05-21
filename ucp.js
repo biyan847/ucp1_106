15da3690808871e1b5d8a39b36adfdef65fe280d
febyan <febyan.putra.ft21@mail.umy.ac.id>
2023-05-21 11:18:31 +0700

    save 1

---
+++ ucp.js
@@ -0,0 +1,8 @@
+// Get elements
+const greetingText = document.getElementById('halo');
+const currentDate = document.getElementById('21 januari');
+
+// Get current date and time
+const now = new Date();
+const hours = now.getHours();
+
